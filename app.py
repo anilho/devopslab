6f562b0250fe97f743015f5e1c73ae067fe0f088
anilho <alicio.cerqueira@gmail.com>
2024-12-05 22:49:44 +0000

    restore message

--- app.py
+++ app.py
@@ -4,4 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
+<<<<<<< HEAD
     return "Hello Mar"        
+=======
+    return "Hello Mars"        
+>>>>>>> 08f4cc9 (Desafio Custom Message)
